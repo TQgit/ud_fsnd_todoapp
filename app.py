95ed5be7b04c640b68d156571c9dbc25cc46ad46
TQ <35425439+TQgit@users.noreply.github.com>
2020-04-30 02:22:37 +0300

    added db migration with flask-migrate and completed column in todos

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
 import sys
 from flask import Flask, render_template, request, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:demo@localhost:5432/todoapp'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f'<todo id:{self.id}, description:{self.description}>'
-
-
-db.create_all()
 
 
 @app.route('/todos/create', methods=['POST'])
